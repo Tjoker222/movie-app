d72ffdba4c1f3f5f16a1aedd2f4507f343b7a4f7
tobias57 <tobias@sthorm.io>
2022-06-21 12:30:33 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss';
+
+
+export function HomePage() {
+  
+
+  return (
+    <>
+      <div className={styles.initial}>
+        <h5>oiasjdoiasj</h5>
+      </div>
+    </>
+  );
+}
